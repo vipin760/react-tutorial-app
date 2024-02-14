32f319d69afebccd0aba412488b5390a0e26c196
vipin_760 <vipinm500@gmail.com>
2024-02-14 12:16:00 +0530

    starting page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,12 @@
 import './App.css';
 import { Component } from 'react';
-import Todo from './component/todo';
-import Counter from './component/counter';
 
 class App extends Component{
-  state={
-    name:"vipin",
-    name2:"hello"
-  }
   render(){
     return (
       <div className='App'>
-        <h1>dhskdfhksd</h1>
-        <Todo myString={this.state.name2} />
-        <Counter/>
-      </div>
+        <h1>react</h1>
+       </div>
     )
   }
 }
